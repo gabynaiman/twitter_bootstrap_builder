5e3b4639c8349191ec3a1348fe47aa9a51273a26
gabriel <gnaiman@keepcon.com>
2012-10-22 12:42:47 -0300

    Append label option for fieldset builder helpers

--- lib/twitter_bootstrap_builder/builders/fieldset_builder.rb
+++ lib/twitter_bootstrap_builder/builders/fieldset_builder.rb
@@ -29,14 +29,14 @@ module TwitterBootstrapBuilder
       end
 
       def text_field(method, options={})
-        ControlGroup.new(model.class.human_attribute_name(method), for: "#{form_builder.object_name}_#{method}") do |cg|
+        ControlGroup.new(options[:label] || model.class.human_attribute_name(method), for: "#{form_builder.object_name}_#{method}") do |cg|
           cg.append form_builder.text_field(method, options)
         end.to_s.html_safe
       end
 
       TwitterBootstrapMarkup::InputSize::VALUES.each do |size|
         define_method "text_field_#{size}" do |method, options={}|
-          ControlGroup.new(model.class.human_attribute_name(method), for: "#{form_builder.object_name}_#{method}") do |cg|
+          ControlGroup.new(options[:label] || model.class.human_attribute_name(method), for: "#{form_builder.object_name}_#{method}") do |cg|
             options.append!(:class, "input-#{size}")
             cg.append form_builder.text_field(method, options)
           end.to_s.html_safe
@@ -44,14 +44,14 @@ module TwitterBootstrapBuilder
       end
 
       def text_area(method, options={})
-        ControlGroup.new(model.class.human_attribute_name(method), for: "#{form_builder.object_name}_#{method}") do |cg|
+        ControlGroup.new(options[:label] || model.class.human_attribute_name(method), for: "#{form_builder.object_name}_#{method}") do |cg|
           cg.append form_builder.text_area(method, options)
         end.to_s.html_safe
       end
 
       TwitterBootstrapMarkup::InputSize::VALUES.each do |size|
         define_method "text_area_#{size}" do |method, options={}|
-          ControlGroup.new(model.class.human_attribute_name(method), for: "#{form_builder.object_name}_#{method}") do |cg|
+          ControlGroup.new(options[:label] || model.class.human_attribute_name(method), for: "#{form_builder.object_name}_#{method}") do |cg|
             options.append!(:class, "input-#{size}")
             cg.append form_builder.text_area(method, options)
           end.to_s.html_safe
@@ -59,49 +59,49 @@ module TwitterBootstrapBuilder
       end
 
       def email_field(method, options={})
-        ControlGroup.new(model.class.human_attribute_name(method), for: "#{form_builder.object_name}_#{method}") do |cg|
+        ControlGroup.new(options[:label] || model.class.human_attribute_name(method), for: "#{form_builder.object_name}_#{method}") do |cg|
           cg.append form_builder.email_field(method, options)
         end.to_s.html_safe
       end
 
       def password_field(method, options={})
-        ControlGroup.new(model.class.human_attribute_name(method), for: "#{form_builder.object_name}_#{method}") do |cg|
+        ControlGroup.new(options[:label] || model.class.human_attribute_name(method), for: "#{form_builder.object_name}_#{method}") do |cg|
           cg.append form_builder.password_field(method, options)
         end.to_s.html_safe
       end
 
       def check_box(method, options={})
-        ControlGroup.new(model.class.human_attribute_name(method), for: "#{form_builder.object_name}_#{method}") do |cg|
+        ControlGroup.new(options[:label] || model.class.human_attribute_name(method), for: "#{form_builder.object_name}_#{method}") do |cg|
           cg.append form_builder.check_box(method, options)
         end.to_s.html_safe
       end
 
       def hidden_field(method, options={})
-        ControlGroup.new(model.class.human_attribute_name(method), for: "#{form_builder.object_name}_#{method}") do |cg|
+        ControlGroup.new(options[:label] || model.class.human_attribute_name(method), for: "#{form_builder.object_name}_#{method}") do |cg|
           cg.append form_builder.hidden_field(method, options)
         end.to_s.html_safe
       end
 
       def select(method, choices, options={}, html_options={})
-        ControlGroup.new(model.class.human_attribute_name(method), for: "#{form_builder.object_name}_#{method}") do |cg|
+        ControlGroup.new(options[:label] || model.class.human_attribute_name(method), for: "#{form_builder.object_name}_#{method}") do |cg|
           cg.append form_builder.select(method, choices, options, html_options)
         end.to_s.html_safe
       end
 
       def date_select(method, options={}, html_options={})
-        ControlGroup.new(model.class.human_attribute_name(method), for: "#{form_builder.object_name}_#{method}") do |cg|
+        ControlGroup.new(options[:label] || model.class.human_attribute_name(method), for: "#{form_builder.object_name}_#{method}") do |cg|
           cg.append form_builder.date_select(method, form_builder.send('objectify_options', options), html_options)
         end.to_s.html_safe
       end
 
       def time_select(method, options={}, html_options={})
-        ControlGroup.new(model.class.human_attribute_name(method), for: "#{form_builder.object_name}_#{method}") do |cg|
+        ControlGroup.new(options[:label] || model.class.human_attribute_name(method), for: "#{form_builder.object_name}_#{method}") do |cg|
           cg.append form_builder.time_select(method, form_builder.send('objectify_options', options), html_options)
         end.to_s.html_safe
       end
 
       def datetime_select(method, options={}, html_options={})
-        ControlGroup.new(model.class.human_attribute_name(method), for: "#{form_builder.object_name}_#{method}") do |cg|
+        ControlGroup.new(options[:label] || model.class.human_attribute_name(method), for: "#{form_builder.object_name}_#{method}") do |cg|
           cg.append form_builder.datetime_select(method, form_builder.send('objectify_options', options), html_options)
         end.to_s.html_safe
       end
